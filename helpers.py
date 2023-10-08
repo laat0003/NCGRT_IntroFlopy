94eeff9210d5b8f64b24a9c74997cb9cdcd2e708
Tariq Laattoe <tariq@ageconsultants.com.au>
2023-10-09 03:42:53 +1000

    Workshop3 Completed

--- helpers.py
+++ helpers.py
@@ -209,3 +209,107 @@ def ws1_mod_trans3(sim,tdis,ims,gwf,modnam):
                                 saverecord=[('HEAD', 'ALL'), ('BUDGET', 'ALL')])
     print('building oc package')
     return
+    
+def ws3_mod1(sim,gwf):
+    """
+    Used to load a SS version of the Flopy GitHub intro model by passing in your own objects.
+
+    Parameters
+    ----------
+    sim : object
+       simulation object created with flopy.mf6.MFSimulation
+    tdis : object
+       simulation timing object created with flopy.mf6.ModflowTdis
+    ims : object
+       simulation solver object created with flopy.mf6.ModflowIms
+    gwf : object
+       simulation model object created with flopy.mf6.ModflowIms
+    chd : object
+       constant head boundary object created with flopy.mf6.ModflowChd
+    Methods
+    ----------
+    None
+    
+    Returns
+    ----------
+    None
+    """    
+    import matplotlib.pyplot as plt
+    name = gwf.name
+    print('building ic package')
+    ic = flopy.mf6.ModflowGwfic(gwf)
+    print('building npf package')
+    npf = flopy.mf6.ModflowGwfnpf(gwf, save_specific_discharge=True)
+
+    budget_file = name + '.bud'
+    head_file = name + '.hds'
+    print('building oc package')
+    oc = flopy.mf6.ModflowGwfoc(gwf,
+                                budget_filerecord=budget_file,
+                                head_filerecord=head_file,
+                                saverecord=[('HEAD', 'ALL'), ('BUDGET', 'ALL')])
+    print('writing simualtion')
+    sim.write_simulation()
+    print('run simualtion')
+    sim.run_simulation()
+    print('extracting heads')
+    head = gwf.output.head().get_data()
+    print('extracting cell-by_cell flows from budget')
+    bud = gwf.output.budget()
+    spdis = bud.get_data(text='DATA-SPDIS')[0]
+    qx, qy, qz = flopy.utils.postprocessing.get_specific_discharge(spdis, gwf)
+    print('building Mapview and plotting')
+    fig,ax = plt.subplots(figsize=(8,5),subplot_kw={'aspect':'equal'})
+    pmv = flopy.plot.PlotMapView(modelgrid=gwf.modelgrid, ax=ax)
+    pmv_head = pmv.plot_array(head)
+    pmv.plot_grid(colors='white')
+    pmv.plot_vector(qx, qy, normalize=True, color="white")
+    plt.colorbar(pmv_head, aspect=30)
+    return()
+
+def ws3_mod1_trans(sim,gwf):
+    """
+    Used to load a SS version of the Flopy GitHub intro model by passing in your own objects.
+
+    Parameters
+    ----------
+    sim : object
+       simulation object created with flopy.mf6.MFSimulation
+    tdis : object
+       simulation timing object created with flopy.mf6.ModflowTdis
+    ims : object
+       simulation solver object created with flopy.mf6.ModflowIms
+    gwf : object
+       simulation model object created with flopy.mf6.ModflowIms
+    chd : object
+       constant head boundary object created with flopy.mf6.ModflowChd
+    Methods
+    ----------
+    None
+    
+    Returns
+    ----------
+    None
+    """    
+    import matplotlib.pyplot as plt
+    name = gwf.name
+    print('building ic package')
+    ic = flopy.mf6.ModflowGwfic(gwf)
+    print('building npf package')
+    npf = flopy.mf6.ModflowGwfnpf(gwf, save_specific_discharge=True)
+    print('building sto package')
+    sto = flopy.mf6.ModflowGwfsto(gwf,save_flows=True,iconvert=1,
+                                  ss=1.0E-05,sy=0.3,steady_state={0: True},
+                                  transient={1: True})
+    budget_file = name + '.bud'
+    head_file = name + '.hds'
+    print('building oc package')
+    oc = flopy.mf6.ModflowGwfoc(gwf,
+                                budget_filerecord=budget_file,
+                                head_filerecord=head_file,
+                                saverecord=[('HEAD', 'ALL'), ('BUDGET', 'ALL')])
+    print('writing simualtion')
+    sim.write_simulation()
+    print('run simualtion')
+    sim.run_simulation()
+    return()
